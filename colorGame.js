fd93630f58b1042ef0d6840a462ba93a46c1d718
JeaneC <volbohel@gmail.com>
2017-07-22 15:46:39 -0400

    Third Commit
    - Game has easy and hard mode
    - Style of game dramatically fixed

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,66 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message")
 var h1 = document.querySelector("h1")
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
 
 colorDisplay.textContent = pickedColor;
 
-console.log(pickedColor)
+resetButton.addEventListener("click", function(){
+  //Generate all new colors
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  messageDisplay.textContent = "";
+  this.textContent = "New Colors"
+
+  for(var i = 0; i<squares.length; i++){
+    squares[i].style.background = colors[i];
+  }
+
+  h1.style.backgroundColor = "steelblue"
+});
+
+easyBtn.addEventListener("click", function(){
+   easyBtn.classList.add("selected");
+   hardBtn.classList.remove("selected");
+
+   numSquares = 3;
+   colors = generateRandomColors(numSquares);
+   pickedColor = pickColor();
+   colorDisplay.textContent = pickedColor;
+
+   for(var i = 0; i < squares.length; i ++){
+     if(colors[i]){
+       squares[i].style.background = colors[i];
+     } else {
+       squares[i].style.display = "none";
+     }
+   }
+});
+
+hardBtn.addEventListener("click", function(){
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  for(var i = 0; i < squares.length; i ++){
+      squares[i].style.background = colors[i];
+      squares[i].style.display = "block";
+  }
+});
+
 
 for(var i=0; i< squares.length; i++){
   //add colors to squares
@@ -22,6 +74,7 @@ for(var i=0; i< squares.length; i++){
       messageDisplay.textContent = "Correct"
       changeColors(pickedColor)
       h1.style.backgroundColor = clickedColor;
+      resetButton.textContent = "Play Again?"
     } else {
       this.style.backgroundColor = "#232323"
       messageDisplay.textContent = "Try Again"
